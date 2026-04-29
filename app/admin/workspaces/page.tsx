ed05c380d5cedb06effc26eb92ba1581c367e60b
huong-dao <d.kimhuong1006@gmail.com>
2026-04-29 13:43:23 +0700

    update something

--- app/admin/workspaces/page.tsx
+++ app/admin/workspaces/page.tsx
@@ -95,7 +95,7 @@ export default function AdminWorkspacesPage() {
                 header: "",
                 cell: (r) => (
                   <Button
-                    className="bg-red-600"
+                    className="bg-red-600 text-white"
                     type="button"
                     size="sm"
                     variant="primary"
